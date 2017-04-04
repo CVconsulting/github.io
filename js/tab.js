3ad4680d28bf0a431f46a056a65731877102b2ed
CVconsulting <cvconsulting@naver.com>
2017-04-04 14:20:05 +0900

    Add files via upload

---
+++ js/tab.js
@@ -0,0 +1,13 @@
+    $(function () {
+
+    $(".map").hide();
+    $(".map:first").show();
+
+    $(".tabList li").click(function () {
+        $(".tabList li").removeClass("on");
+        $(this).addClass("on");
+        $(".map").hide();
+        var activeTab = $(this).attr("rel");
+        $("#" + activeTab).show();
+    });
+});
