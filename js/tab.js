2b4f4a55102cf63c5d0d2526b64fc4b4f8203bf6
CVconsulting <cvconsulting@naver.com>
2017-04-04 14:50:15 +0900

    Update tab.js

--- js/tab.js
+++ js/tab.js
@@ -6,8 +6,8 @@
     $(".tabList li").click(function () {
         $(".tabList li").removeClass("on");
         $(this).addClass("on");
+        $(".map").hide();
         var activeTab = $(this).attr("rel");
         $("#" + activeTab).show();
-        $(".map").hide();
     });
 });
